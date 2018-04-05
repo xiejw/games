41f1fb97782a4a0456a764f8170ffd7f4c9e1953
Jianwei Xie <xiejw0217@gmail.com>
2018-04-05 12:20:24 -0700

    Add testing.

---
+++ five-in-a-row/Testing/Board.swift
@@ -0,0 +1,45 @@
+import XCTest
+
+class BoardTest : XCTestCase {
+  
+  func testMoveEquality() {
+    let move1 = Move(x: 2, y: 3)
+    let move2 = Move(x: 2, y: 3)
+    XCTAssertTrue(move1 == move2)
+    
+    let move3 = Move(x: 3, y: 2)
+    XCTAssertFalse(move1 == move3)
+  }
+  
+  func testStateEquality() {
+    let move23 = Move(x: 2, y: 3)
+    let move33 = Move(x: 3, y: 3)
+    let move43 = Move(x: 4, y: 3)
+    
+    // Black 2 3
+    var state1 = State(move23)
+    // White 3 3
+    state1 = State(move33, state1)
+    // Black 4 3
+    state1 = State(move43, state1)
+  
+    // state2 is same as state1 but swapping the move1 and move3.
+    // They both represent the black. So, state1 == state2
+    // Black 4 3
+    var state2 = State(move43)
+    // White 3 3
+    state2 = State(move33, state2)
+    // Black 2 3
+    state2 = State(move23, state2)
+    XCTAssertTrue(state1 == state2)
+    
+    // Swapping move1 and move2 are not ok as the colors are different.
+    // Black 3 3
+    var state3 = State(move33)
+    // White 2 3
+    state3 = State(move23, state3)
+    // Black 4 3
+    state3 = State(move43, state3)
+    XCTAssertFalse(state1 == state3)
+  }
+}
