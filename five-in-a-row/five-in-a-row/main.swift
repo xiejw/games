09f72182b06d9c2a44ce07431d564c89d8f4eb2d
Jianwei Xie <xiejw0217@gmail.com>
2018-05-22 15:39:53 -0700

    Do a another human-vs-mac test.

--- five-in-a-row/five-in-a-row/main.swift
+++ five-in-a-row/five-in-a-row/main.swift
@@ -3,14 +3,14 @@ import Foundation
 struct Configuration {
     var size: Int
     var numberToWin: Int
-    var selfPlayTimeInSecs: Double
+    var selfPlayTimeInSecs: Double?
     var perMoveSimulationTimes: Int
     var recordStates: Bool
     var vsHuman: Bool
     var ratingStage: Bool
     var verbose: Int
 
-    init(size: Int = 8, numberToWin: Int = 5, selfPlayTimeInSecs: Double, perMoveSimulationTimes: Int = 1600, recordStates: Bool = false, vsHuman: Bool = false, ratingStage: Bool = false, verbose: Int = 0) {
+    init(size: Int = 8, numberToWin: Int = 5, selfPlayTimeInSecs: Double? = nil, perMoveSimulationTimes: Int = 1600, recordStates: Bool = false, vsHuman: Bool = false, ratingStage: Bool = false, verbose: Int = 0) {
         self.size = size
         self.numberToWin = numberToWin
         self.selfPlayTimeInSecs = selfPlayTimeInSecs
@@ -27,35 +27,31 @@ let configuration: Configuration
 if let value = ProcessInfo.processInfo.environment["RL_TIME_IN_SECS"] {
     print("RL mode.")
     configuration = Configuration(selfPlayTimeInSecs: Double(value)!, recordStates: true)
-} else if let value = ProcessInfo.processInfo.environment["RATING_TIME_IN_SECS"] {
-    print("Rating mode.")
-    configuration = Configuration(selfPlayTimeInSecs: Double(value)!,
-                                  ratingStage: true)
-} else {
-    configuration = Configuration(selfPlayTimeInSecs: 3.0, vsHuman: true, verbose: 1)
-}
-
-print("Configuration:\n\(configuration)")
-
-if configuration.ratingStage {
-    selfPlaysAndRating(size: configuration.size,
-                       numberToWin: configuration.numberToWin,
-                       selfPlayTimeInSecs: configuration.selfPlayTimeInSecs,
-                       perMoveSimulationTimes: configuration.perMoveSimulationTimes,
-                       verbose: configuration.verbose)
-    exit(0)
-}
-
-if !configuration.vsHuman {
+    print("Configuration:\n\(configuration)")
     selfPlaysAndRecord(size: configuration.size,
                        numberToWin: configuration.numberToWin,
-                       selfPlayTimeInSecs: configuration.selfPlayTimeInSecs,
+                       selfPlayTimeInSecs: configuration.selfPlayTimeInSecs!,
                        perMoveSimulationTimes: configuration.perMoveSimulationTimes,
                        recordStates: configuration.recordStates,
                        verbose: configuration.verbose)
     exit(0)
 }
 
+if let value = ProcessInfo.processInfo.environment["RATING_TIME_IN_SECS"] {
+    print("Rating mode.")
+    configuration = Configuration(selfPlayTimeInSecs: Double(value)!,
+                                  ratingStage: true)
+    print("Configuration:\n\(configuration)")
+    selfPlaysAndRating(size: configuration.size,
+                       numberToWin: configuration.numberToWin,
+                       selfPlayTimeInSecs: configuration.selfPlayTimeInSecs!,
+                       perMoveSimulationTimes: configuration.perMoveSimulationTimes,
+                       verbose: configuration.verbose)
+    exit(0)
+}
+
+configuration = Configuration(vsHuman: true, verbose: 1)
+print("Configuration:\n\(configuration)")
 // Play with human
 playWithHuman(size: configuration.size,
               numberToWin: configuration.numberToWin,
