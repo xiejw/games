a53ae870aa0bd1ff26d85e38721a38cf2db2343e
Jianwei Xie <xiejw0217@gmail.com>
2018-05-24 18:03:22 -0700

    Remove random policy for comparison.

--- five-in-a-row/five-in-a-row/policy/premade_policies.swift
+++ five-in-a-row/five-in-a-row/policy/premade_policies.swift
@@ -1,23 +1,24 @@
 import Foundation
 
 func getPremadePolicisToRating(size: Int, numberToWin _: Int, board: Board, perMoveSimulationTimes: Int, verbose _: Int = 0) -> [() -> [Policy]] {
-    func policyFn1() -> [Policy] {
-        let mctsPolicy = MCTSBasedPolicy(name: "mcts", size: size,
-                                         predictor: DistributionPredictionWrapper(size: size),
-                                         board: board, perMoveSimulationTimes: perMoveSimulationTimes)
-        let randomPolicy = RandomPolicy(name: "random")
-        return [mctsPolicy, randomPolicy]
-    }
-
-    func policyFn2() -> [Policy] {
-        let mctsPolicy = MCTSBasedPolicy(name: "mcts", size: size,
-                                         predictor: DistributionPredictionWrapper(size: size),
-                                         board: board, perMoveSimulationTimes: perMoveSimulationTimes)
-        let randomPredictorPolicy = MCTSBasedPolicy(name: "mcts_random_predictor", size: size,
-                                                    predictor: RandomPredictor(size: size),
-                                                    board: board, perMoveSimulationTimes: perMoveSimulationTimes)
-        return [mctsPolicy, randomPredictorPolicy]
-    }
+    // Once the ckpt is good enough, we do not compare with random stuff anymore.
+//    func policyFn1() -> [Policy] {
+//        let mctsPolicy = MCTSBasedPolicy(name: "mcts", size: size,
+//                                         predictor: DistributionPredictionWrapper(size: size),
+//                                         board: board, perMoveSimulationTimes: perMoveSimulationTimes)
+//        let randomPolicy = RandomPolicy(name: "random")
+//        return [mctsPolicy, randomPolicy]
+//    }
+//
+//    func policyFn2() -> [Policy] {
+//        let mctsPolicy = MCTSBasedPolicy(name: "mcts", size: size,
+//                                         predictor: DistributionPredictionWrapper(size: size),
+//                                         board: board, perMoveSimulationTimes: perMoveSimulationTimes)
+//        let randomPredictorPolicy = MCTSBasedPolicy(name: "mcts_random_predictor", size: size,
+//                                                    predictor: RandomPredictor(size: size),
+//                                                    board: board, perMoveSimulationTimes: perMoveSimulationTimes)
+//        return [mctsPolicy, randomPredictorPolicy]
+//    }
 
     func policyFn3() -> [Policy] {
         let mctsPolicy = MCTSBasedPolicy(name: "mcts", size: size,
@@ -29,5 +30,6 @@ func getPremadePolicisToRating(size: Int, numberToWin _: Int, board: Board, perM
         return [mctsPolicy, mctsLastIterationPolicy]
     }
 
-    return [policyFn1, policyFn2, policyFn3]
+    // return [policyFn1, policyFn2, policyFn3]
+    return [policyFn3]
 }
