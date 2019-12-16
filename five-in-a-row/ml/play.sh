9ddd31be5a70befb61aeb52f1d4c3f2e49b6eeb6
Jianwei Xie <xiejw0217@gmail.com>
2019-12-16 07:41:14 -0500

    Clean up the foldder.

--- five-in-a-row/ml/play.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd ..
-xcodebuild -project five-in-a-row.xcodeproj   build
-script -q /dev/null /Users/xiejw/Workspace/games/five-in-a-row/build/Release/five-in-a-row 2>&1 | tee /tmp/play.txt
-cd ml
-
