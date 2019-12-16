674a771e2c32870d92b8e97bdbf217956d7f0050
Jianwei Xie <xiejw0217@gmail.com>
2019-12-16 16:50:51 -0500

    Started adding exp_buffer.

---
+++ connect_4/data/experience_buffer.py
@@ -0,0 +1,18 @@
+
+# This class assumes 'b' and 'w' play in turns. No passes.
+class ExperienceBuffer(object):
+
+    def __init__(self):
+        self._moves = []
+        self._current_epoch_moves = []
+
+    def start_epoch(self):
+        pass
+
+    # Winner == None means a tie
+    def end_epoch(self, winner):
+        pass
+
+    def add_move(self, position_pair, color):
+        pass
+
