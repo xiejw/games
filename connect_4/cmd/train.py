44197510ceadc0085e834e7c805a6f830c13b49c
Jianwei Xie <xiejw0217@gmail.com>
2019-12-18 10:07:20 -0500

    Added model features.

--- connect_4/cmd/train.py
+++ connect_4/cmd/train.py
@@ -1,11 +1,9 @@
 import random
-import numpy as np
 
 from data.sql import read_records
 from data import TrainingState
+from data import convert_states_to_model_features
 from game import GameConfig
-from game import Color
-from game import Position
 
 ###########################
 ### Configuration to change
@@ -30,35 +28,13 @@ states = [TrainingState.parsing(config, x) for x in records]
 print("Shuffling")
 random.shuffle(states)  # inplace
 
-# rewards_np = np.array([x.reward for x in states])
-# position_np = np.array([config.convert_position_to_index(x.position) for x in
-#         states])
-#
-# print([x.position.__str__() for x in states])
-# print(position_np)
-# print(rewards_np)
+print("Converting to feature planes.")
+features, labels = convert_states_to_model_features(config, states)
+boards_np = features
+rewards_np, positions_np = labels
 
-# 3 is the number of feature planes.
-boards_np = np.zeros([len(states), 3, config.rows, config.columns])
-for i, state in enumerate(states):
-    print(state)
-    if state.next_player_color == Color.BLACK:
-        boards_np[i, 2, :, :] = 1.0
-
-    snapshot = state.snapshot
-    for x in range(config.rows):
-        for y in range(config.columns):
-            color = snapshot.get(Position(x, y))
-            if color is None:
-                continue
-
-            if color == Color.BLACK:
-                boards_np[i, 0, x, y] = 1.0
-            else:
-                assert color == Color.WHITE
-                boards_np[i, 1, x, y] = 1.0
-
-print(boards_np)
-boards_np = np.transpose(boards_np, [0, 2, 3, 1])
-print(boards_np)
-
+print("Print samples.")
+print("Original State:", states[0])
+print("Board Numpy Array:", boards_np[0])
+print("Reward Numpy:", rewards_np[0], "dtype", rewards_np.dtype)
+print("Position Int:", positions_np[0], "dtype:", positions_np.dtype)
--- connect_4/data/__init__.py
+++ connect_4/data/__init__.py
@@ -2,3 +2,4 @@ from .experience_buffer import ExperienceBuffer
 from .state import State
 from .state import InferenceState
 from .state import TrainingState
+from .model_features import convert_states_to_model_features
