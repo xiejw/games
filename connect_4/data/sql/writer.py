38778e6a9236f351db76c65b7a1963d16fffd25c
Jianwei Xie <xiejw0217+xiejw2g@gmail.com>
2019-12-25 17:11:38 -0800

    Have mysql retry logic.

--- connect_4/data/sql/writer.py
+++ connect_4/data/sql/writer.py
@@ -1,9 +1,27 @@
+import sys
+
 from .sql_db import _mydb
 
 _sql = "INSERT INTO records (state) VALUES (%s)"
 
 def store_record(record_str):
-    _mycursor = _mydb.cursor()
+    # The connection might be offline already due to some reasons. So have retry
+    # logic here.
+    i = 0
+    while True:
+        try:
+            _mycursor = _mydb.cursor()
+            break
+        except:
+            print("===== Unexpected db error", sys.exc_info()[0])
+            i += 1
+            if i > 3:
+                raise RuntimeError("Exceed retries times.")
+
+            print("Retrying")
+            _mydb.reconnect()
+
+
     val = (record_str,)
     _mycursor.execute(_sql, val)
     _mydb.commit()
