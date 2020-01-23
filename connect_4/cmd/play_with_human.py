7a147ff277c6762347d5444136bda157bf87d1f0
Jianwei Xie <xiejw0217@gmail.com>
2020-01-23 10:40:58 -0800

    Moved the suppress warning to util.

--- connect_4/cmd/play_with_human.py
+++ connect_4/cmd/play_with_human.py
@@ -1,21 +1,5 @@
-# Disable all warnings from TensorFlow (Python side).
-#
-# - The order to disable warnings matters.
-# - For CPP side, set # TF_CPP_MIN_LOG_LEVEL=3.
-
-import warnings
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore",category=FutureWarning)
-
-    from tensorflow.python.util import deprecation
-    deprecation._PRINT_DEPRECATION_WARNINGS = False
-
-    import tensorflow as tf
-
-    # In addition, disable warning logging.
-    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
-    from tensorflow import keras
+import util
+util.suppress_tf_warning()
 
 ##########################
 # Real fun code starts.
