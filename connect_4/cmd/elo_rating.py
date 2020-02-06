106fb9e4f0273059ea0a7f768a21f873687fef87
Jianwei Xie <xiejw0217@gmail.com>
2020-02-06 12:36:00 -0800

    Added elo_rating.

--- connect_4/cmd/elo_rating.py
+++ connect_4/cmd/elo_rating.py
@@ -1,50 +1,77 @@
 import math
+import collections
 
-# Function to calculate the Probability
-def Probability(rating1, rating2):
+# Function to calculate the prob_winning.
+def prob_winning(rating1, rating2):
     return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400))
 
 
-# Function to calculate Elo rating
-# K is a constant.
-# d determines whether
-# Player A wins or Player B.
-def EloRating(Ra, Rb, K, d):
+# Function to calculate Elo rating after updating.
+#
+# - K is a constant.
+# - d determines whether Player A wins or Player B.
+#     1.0 means A wins.
+#     0.5 means tie.
+#     0.0 means B wins.
+def update_elo_rating(Ra, Rb, K, d):
+    Pa = prob_winning(Rb, Ra)
+    Pb = prob_winning(Ra, Rb)
 
-
-    # To calculate the Winning
-    # Probability of Player B
-    Pb = Probability(Ra, Rb)
-
-    # To calculate the Winning
-    # Probability of Player A
-    Pa = Probability(Rb, Ra)
-
-    # Case -1 When Player A wins
-    # Updating the Elo Ratings
-    if (d == 1) :
-        Ra = Ra + K * (1 - Pa)
-        Rb = Rb + K * (0 - Pb)
-
-
-    # Case -2 When Player B wins
-    # Updating the Elo Ratings
-    else :
-        Ra = Ra + K * (0 - Pa)
-        Rb = Rb + K * (1 - Pb)
-
-
-    print("Updated Ratings:-")
-    print("Ra =", round(Ra, 6)," Rb =", round(Rb, 6))
+    Ra = Ra + K * (d - Pa)
+    Rb = Rb + K * ((1 - d) - Pb)
     return Ra, Rb
 
-# Driver code
-
-# Ra and Rb are current ELO ratings
-Ra = 1000
-Rb = 1000  # anchored.
 K = 30
-d = 1
 
-for _ in range(3000):
-    Ra, _ = EloRating(Ra, Rb, K, d)
+# For key i, it is the result the Iter i against Iter (i-1).
+RESULTS = collections.OrderedDict()
+RESULTS[1] = {'win': 29, 'lost':  8, 'tie':  3}
+RESULTS[2] = {'win': 31, 'lost':  6, 'tie':  3}
+RESULTS[3] = {'win': 23, 'lost':  5, 'tie': 12}
+
+elo_r = collections.OrderedDict()
+elo_r[0] = 1000.  # Anchored.
+
+for (k, v) in RESULTS.items():
+    assert k >= 1
+    assert k not in elo_r
+
+    # Anchor elo_r[k] to elo_r[k-1] first
+    elo_r[k] = elo_r[k - 1]
+
+    result = RESULTS[k]
+
+    # The algorithrm is simple and partially deterministic.
+    # We go over the loop, and in each iteration, one win, lost and tie are
+    # updated in the deterministic order. If the wins have reach the cap,
+    # specified by the `RESULTS`, it is skipped. Same applies to lost and tie.
+    win = 0
+    lost = 0
+    tie = 0
+
+    while True:
+        changed = False
+
+        # We do not update elo rating for k-1.
+        if win < result['win']:
+            changed = True
+            win += 1
+            elo_r[k], _ = update_elo_rating(elo_r[k], elo_r[k-1], K, 1.0)
+
+        if lost < result['lost']:
+            changed = True
+            lost += 1
+            elo_r[k], _ = update_elo_rating(elo_r[k], elo_r[k-1], K, 0.0)
+
+        if tie < result['tie']:
+            changed = True
+            tie += 1
+            elo_r[k], _ = update_elo_rating(elo_r[k], elo_r[k-1], K, 0.5)
+
+        if not changed:
+            break
+
+    if k == 1:
+      print("Elo rating for model {}: {:.2f}".format(0, elo_r[0]))
+
+    print("Elo rating for model {}: {:.2f}".format(k, elo_r[k]))
