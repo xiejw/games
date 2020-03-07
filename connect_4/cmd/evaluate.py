059731cf51e6d97fc4ed8e4afedd370d4fd1a019
Jianwei Xie <xiejw0217+xiejw2g@gmail.com>
2020-03-06 16:01:29 -0800

    Added result for iter 3.

--- connect_4/cmd/evaluate.py
+++ connect_4/cmd/evaluate.py
@@ -29,6 +29,7 @@ WEIGHTS = [
         '.build/weights.h5-1583173597',  # Mar  2 (10:28), 2020. Iter  0 random
         '.build/weights.h5-1583244370',  # Mar  3 (06:06), 2020. Iter  1
         '.build/weights.h5-1583331870',  # Mar  4 (06:24), 2020. Iter  2
+        '.build/weights.h5-1583437368',  # Mar  5 (11:42), 2020. Iter  3
 ]
 
 WEIGHTS_FILE_1 = WEIGHTS[-2]
