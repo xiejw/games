29db3c81fe0b1d9d819d9b8833b0621559be5462
Jianwei Xie <xiejw0217@gmail.com>
2020-05-09 17:57:10 -0700

    Added model folder.

---
+++ gomoku/lib/model/cnn.py
@@ -0,0 +1,32 @@
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+
+# Builds a simple CNN model. Modifed from Keras MNIST CNN.
+def build_cnn_model(input_shape, num_classes):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(4, 4),
+                     activation='relu',
+                     padding='same',
+                     input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    def categorical_accuracy(y_true, y_pred):
+        # We flipped the value for the position vector. So, use abshere.
+        y_true = keras.backend.abs(y_true)
+        return keras.metrics.categorical_accuracy(y_true, y_pred)
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.SGD(lr=0.001, clipnorm=1.0),
+                  metrics=[categorical_accuracy])
+
+    return model
---
+++ gomoku/lib/model/resnet.py
@@ -0,0 +1,114 @@
+import keras
+
+from keras import backend as K
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, Add
+from keras.layers import Conv2D, BatchNormalization, ReLU
+from keras.regularizers import l2
+
+L2_WEIGHT_DECAY = 1e-4
+
+
+# Builds a multi-layer Residual network.
+def build_resnet_model(input_shape, num_classes, num_layers=5):
+    inp = Input(shape=input_shape)
+
+    x = inp
+
+    # Conv block
+    x = Conv2D(128,
+               kernel_size=(4, 4),
+               padding='same',
+               kernel_regularizer=l2(L2_WEIGHT_DECAY))(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+
+    # Residual blocks
+    for _ in range(num_layers):
+      x = build_residual_block(x)
+
+    head_base = x
+    del x
+
+    # Build heads
+    policy_head = build_policy_head(head_base, num_classes, head_name='policy')
+    value_head = build_value_head(head_base, head_name='value')
+
+    # Build model.
+    model = Model(inputs=[inp], outputs=[policy_head, value_head])
+
+    def abs_metrics(y_true, y_pred):
+        return K.mean(K.abs(y_true - y_pred), axis=-1)
+
+    model.compile(
+        loss={'policy': 'categorical_crossentropy', 'value': 'mse'},
+        optimizer=keras.optimizers.SGD(lr=0.001, momentum=0.9),
+        metrics={'policy': 'categorical_accuracy', 'value' :abs_metrics})
+
+    return model
+
+
+def build_residual_block(inp):
+    x = inp
+    x = Conv2D(
+            128,
+            kernel_size=(4, 4),
+            padding='same',
+            kernel_regularizer=l2(L2_WEIGHT_DECAY))(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+
+    x = Conv2D(
+            128,
+            kernel_size=(4, 4),
+            padding='same',
+            kernel_regularizer=l2(L2_WEIGHT_DECAY))(x)
+    x = BatchNormalization()(x)
+
+    x = Add()([x, inp])
+    x = ReLU()(x)
+
+    return x
+
+
+def build_policy_head(head_base, num_classes, head_name):
+    x = Conv2D(
+            2,
+            kernel_size=(1, 1),
+            padding='same',
+            kernel_regularizer=l2(L2_WEIGHT_DECAY))(head_base)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    x = Flatten()(x)
+
+    policy_head = Dense(
+        num_classes,
+        activation='softmax',
+        name=head_name,
+        kernel_regularizer=l2(L2_WEIGHT_DECAY),
+        bias_regularizer=l2(L2_WEIGHT_DECAY))(x)
+    return policy_head
+
+
+def build_value_head(head_base, head_name):
+    x = Conv2D(
+            2,
+            kernel_size=(1, 1),
+            padding='same',
+            kernel_regularizer=l2(L2_WEIGHT_DECAY))(head_base)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    x = Flatten()(x)
+    x = Dense(
+            256,
+            activation='relu',
+            kernel_regularizer=l2(L2_WEIGHT_DECAY),
+            bias_regularizer=l2(L2_WEIGHT_DECAY))(x)
+
+    value_head = Dense(
+            1,
+            activation='tanh',
+            name=head_name,
+            kernel_regularizer=l2(L2_WEIGHT_DECAY),
+            bias_regularizer=l2(L2_WEIGHT_DECAY))(x)
+    return value_head
