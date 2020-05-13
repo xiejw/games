1397e5868383e47abb663f2ffdc81cafd130d208
Jianwei Xie <xiejw0217@gmail.com>
2020-05-12 17:07:00 -0700

    Added more check.

--- gomoku/cmd/hparams_cli.py
+++ gomoku/cmd/hparams_cli.py
@@ -3,6 +3,7 @@ import hparams
 print(hparams)
 
 pd = {
+    'current_loop_index': 0,
     'training_total_loops': 10,
     'training_games_per_loop': 600,
     'training_samples_per_game': 50,
--- gomoku/lib/hparams/controller.py
+++ gomoku/lib/hparams/controller.py
@@ -3,6 +3,7 @@ from .redis import get_redis_handler
 _INT_TYPE = 0
 
 _KEYS = [
+        ('current_loop_index', _INT_TYPE),
         ('training_total_loops', _INT_TYPE),
         ('training_games_per_loop', _INT_TYPE),
         ('training_samples_per_game', _INT_TYPE),
@@ -19,7 +20,7 @@ def get_hparams():
         if type_id == _INT_TYPE:
             params_dict[key] = int(v)
         else:
-            raise TypeError("Unsupported type {}".format(type_id))
+            raise TypeError('Unsupported type {}'.format(type_id))
 
     r.close()
 
@@ -30,7 +31,10 @@ def set_hparams(params_dict):
 
     for key_type_pair in _KEYS:
         key, _ = key_type_pair
-        r.set(key, params_dict[key])
+        v = params_dict.get(key)
+        if v is None:
+            raise RuntimeError('Key cannot be found in hparams: {}'.format(key))
+        r.set(key, v)
 
     r.close()
 
