4f5fb172deeb511a6c42f1fcfa32b7e9016312d9
Jianwei Xie <xiejw0217@gmail.com>
2020-06-13 14:14:19 -0700

    refactored the code.

--- black_and_white/play.py
+++ black_and_white/play.py
@@ -1,76 +1,83 @@
+import copy
+
 import Tkinter as tk
 
-dim = 8
+class Board(object):
 
-board = [ [None]* dim for _ in range(dim) ]
-widgets = [[None]*dim for _ in range(dim) ]
-default_colors = [
-        [
-    'lightyellow2' if (i + j) % 2 == 0 else 'light grey' for j in range(dim)] for i in range(dim)]
+    def __init__(self, size, root):
+        self._frame = self._setUpFrame(size, root)
+        self._bg_colors = self._setUpBgColors(size)
+        self._colors = copy.deepcopy(self._bg_colors)
+        self._widgets = self._setUpWidgets(size, self._frame)
+        self._history = []
+        self._size = size
 
+    def _setUpFrame(self, size, root):
+        frame = tk.Frame(root)
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+        frame.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W+tk.E)
+
+        for i in range(size):
+            frame.columnconfigure(i, pad=3, weight=1)
+            frame.rowconfigure(i, pad=3, weight=1)
+        return frame
+
+    def _setUpBgColors(self, size):
+        def color_fn(i, j):
+            return 'lightyellow2' if (i + j) % 2 == 0 else 'light grey'
+        return [[color_fn(i, j) for j in range(size) ] for i in range(size)]
+
+    def _setUpWidgets(self, size, frame):
+        widgets = [[None for _ in range(size)] for _ in range(size)]
+        for i in range(size):
+            for j in range(size):
+                bg_color = self._bg_colors[i][j]
+                L = tk.Label(frame, text='    ', bg=bg_color)
+                L.grid(row=i, column=j, sticky=tk.N+tk.S+tk.W+tk.E)
+
+                L.bind('<Button-1>',lambda e,i=i,j=j: self.on_click(i,j,e))
+                L.bind('<Key>', self.on_key)
+                widgets[i][j] = L
+        return widgets
+
+    def on_click(self, i, j, event):
+        del event
+        self._flip_one_spot(i, j)
+        self._history.append((i,j))
+
+    def _flip_one_spot(self, i, j):
+        pairs = [(i,j), (i-1,j), (i+1,j), (i,j-1), (i,j+1)]
+
+        size = self._size
+        for p in pairs:
+            x, y = p
+            if x < 0 or x >= size or y < 0 or y>= size:
+                continue
+
+            c = self._colors[x][y]
+            c = self._bg_colors[x][y] if c == 'black' else 'black'
+            self._colors[x][y] = c
+
+            self._widgets[x][y].config(bg=c)
+            self._widgets[x][y].focus_set()  # to recv key
+
+    # Label must get focus_set()
+    def on_key(self, e):
+        if e.char != ' ':
+            return
+        if not self._history:
+            return
+
+        i, j = self._history[-1]
+        self._flip_one_spot(i, j)
+        self._history.pop(-1)
+
+size = 8
 
 root = tk.Tk()
 root.geometry("400x400")
 
-history = []
-
-def on_click(i,j,event):
-    del event
-    pairs = [(i,j), (i-1,j), (i+1,j), (i,j-1), (i,j+1)]
-
-    history.append((i,j))
-
-    for p in pairs:
-        x, y = p
-        if x < 0 or x >= dim or y < 0 or y>= dim:
-            continue
-
-
-        c = board[x][y]
-        default_c = default_colors[x][y]
-        c = default_c if c == 'black' else 'black'
-        widgets[x][y].config(bg=c)
-        widgets[x][y].focus_set()  # to recv key
-        board[x][y] = c
-
-frame = tk.Frame(root)
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-frame.grid(row=0, column=0, sticky= tk.N + tk.S + tk.W + tk.E)
-
-def key(e):
-    if e.char != ' ':
-        return
-    # Label must get focus_set()
-    if not history:
-        return
-
-    i, j = history[-1]
-    on_click(i,j, None)
-    history.pop(-1)
-    history.pop(-1)  # twice
-
-
-frame.bind('<Key>' , key)
-
-for i in range(dim):
-    frame.columnconfigure(i, pad=3, weight=1)
-    frame.rowconfigure(i, pad=3, weight=1)
-
-for i,row in enumerate(board):
-    for j,column in enumerate(row):
-        args = {}
-        args['sticky'] = tk.N + tk.S + tk.W + tk.E
-
-        L = tk.Label(frame,text='    ',bg='grey')
-        L.grid(row=i,column=j, **args)
-        L.bind('<Button-1>',lambda e,i=i,j=j: on_click(i,j,e))
-        L.bind('<Key>', key)
-
-        c = default_colors[i][j]
-        L.config(bg=c)
-        widgets[i][j] = L
-        board[i][j] =c
+Board(size, root)
 
 root.mainloop()
