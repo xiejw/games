623554c6d1a9913aa1a82705205beeb078ff38f0
Jianwei Xie <xiejw0217@gmail.com>
2020-08-23 17:34:00 -0700

    print col title.

--- ui/src/board_view.rs
+++ ui/src/board_view.rs
@@ -85,6 +85,21 @@ impl cursive::view::View for BoardView {
                 printer.print((x, y), text)
             });
         }
+
+        // Print column title.
+        for x in 0..self.board.size.x {
+            let y = 0;
+            let text = match x {
+                10 => "a ",
+                11 => "b ",
+                12 => "c ",
+                13 => "d ",
+                14 => "e ",
+                15 => "f ",
+                _ => ["0 ", "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 "][x],
+            };
+            printer.print((x * 2 + BoardView::LEFT_MARGIN, y), text);
+        }
     }
 
     fn take_focus(&mut self, _: Direction) -> bool {
